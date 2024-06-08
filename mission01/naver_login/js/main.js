5763514ea02fe032a14488d04ca64d1eb4ffa8cb
hammadam <hammadam@naver.com>
2024-06-08 18:28:15 +0900

    main.js 주석 정리

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -25,13 +25,7 @@ function pwReg(text) {
 }
 
 //Mission-0
-
-/*
-**문제:** 객체에서 특정 키의 값을 안전하게 가져오는 함수를 작성하세요.
-
-**설명:** 객체와 키를 인수로 받아, 객체에 해당 키가 존재하면 그 키에 해당하는 값을 반환하고, 존재하지 않으면 에러를 발생시키는 함수를 작성하세요.
-
-*/
+//문제:객체에서 특정 키의 값을 안전하게 가져오는 함수를 작성하세요.
 
 const person = {
   name: "Alice",
@@ -46,7 +40,6 @@ function getValueAtObject(obj, key) {
     throw new Error(
       `Error ! getValueAtObject 함수에는 알맞은 key를 입력해주세요. person에는 ${key}의 값이 없습니다.`
     );
-    //console.log(`Error ! getValueAtObject 함수에는 알맞은 key를 입력해주세요. person에는 ${key}의 값이 없습니다.`);
   }
 }
 
@@ -55,17 +48,7 @@ console.log(getValueAtObject(person, "age")); // 25
 console.log(getValueAtObject(person, "city")); // 'Wonderland'
 console.log(getValueAtObject(person, "country")); // Error !
 
-/*
-**문제:** 배열에서 특정 인덱스의 값을 안전하게 가져오는 함수를 작성하세요.
-
-**설명:** 배열과 인덱스를 인수로 받아, 인덱스가 배열의 유효한 범위 내에 있으면 그 인덱스에 해당하는 값을 반환하고, 유효하지 않은 인덱스일 경우 에러 메시지를 반환하는 함수를 작성하세요.
-
-추가설명
-- 배열의 인덱스는 0부터 시작하며, 주어진 인덱스가 배열의 유효한 범위 내에 있는지 확인해야 합니다.
-- 인수로 받은 대상이 배열이 아닐 경우 에러를 생성하고 생성한 에러를 반환해야 합니다.
-- 유효한 인덱스일 경우 해당 인덱스의 값을 반환하고, 그렇지 않으면  에러를 생성하고 생성한 에러를 반환해야 합니다.
-*/
-console.log("--문제2--");
+//문제:배열에서 특정 인덱스의 값을 안전하게 가져오는 함수를 작성하세요.
 const numbers = [10, 20, 30, 40, 50];
 
 function getNumberAtArray(arr, index) {
@@ -75,7 +58,6 @@ function getNumberAtArray(arr, index) {
     }
   } else {
     throw new Error(`Error ! numbers 배열에는 ${index}번째 값이 없습니다.`);
-    //console.log(`Error ! numbers 배열에는 ${index}번째 값이 없습니다.`);
   }
 }
 
